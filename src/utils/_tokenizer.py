bc3c013ff4988294e34a525553ce3ab9fe2ebe7f
CiCi <96775840+weecici@users.noreply.github.com.>
2025-11-12 20:41:05 +0700

    fix(utils): use nltk for word tokenize

--- src/utils/_tokenizer.py
+++ src/utils/_tokenizer.py
@@ -1,11 +1,13 @@
 import nltk
-import bm25s
-from bm25s.tokenization import Tokenized
+import string
 from nltk import WordNetLemmatizer, SnowballStemmer, word_tokenize
+from nltk.corpus import stopwords
 from typing import Literal
 
 
 nltk.download("wordnet")
+_stopwords = set(stopwords.words("english"))
+_punctuations: set[str] = set([p for p in string.punctuation])
 
 
 def stem(words: list[str]) -> list[str]:
@@ -21,11 +23,21 @@ def lemmatize(words: list[str]) -> list[str]:
 def tokenize(
     texts: list[str],
     word_process_method: Literal["lemmatize", "stem"] = "stem",
-) -> list[list[str]] | Tokenized:
+) -> list[list[str]]:
     process_method: callable[[list[str]], list[str]] = stem
     if word_process_method == "lemmatize":
         process_method = lemmatize
-    text_tokens = bm25s.tokenize(
-        texts=texts, stopwords="en", stemmer=process_method, return_ids=False
-    )
-    return text_tokens
+
+    tok_lists = [word_tokenize(text) for text in texts]
+
+    sw_punc_rm_tok_lists = [
+        [
+            token
+            for token in tokens
+            if not (token.lower() in _stopwords or token in _punctuations)
+        ]
+        for tokens in tok_lists
+    ]
+
+    final_tok_lists = [process_method(tokens) for tokens in sw_punc_rm_tok_lists]
+    return final_tok_lists
